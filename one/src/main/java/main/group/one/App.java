140a720df4ad9592a2ccb789eb68ffa878b914d0
anuj4ufriends <anuj4ufriends@gmail.com>
2015-06-01 09:37:45 -0400

    Commit 1

---
+++ one/src/main/java/main/group/one/App.java
@@ -0,0 +1,13 @@
+package main.group.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
